e9e1e4976da42b387790544845f90ef10d0757fd
Acewey <ouassimraw@gmail.com>
2023-03-18 15:13:09 +0000

    light login register

---
+++ login-Register light/script.js
@@ -0,0 +1,12 @@
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("sign-contain");
+
+registerButton.addEventListener("click", () =>{
+  container.classList.add("right-panel-active");
+});
+
+
+loginButton.addEventListener("click", () =>{
+  container.classList.remove("right-panel-active");
+});
